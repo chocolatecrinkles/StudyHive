19abee5aa564db1dc66aafe8018f489fd41b5626
MichaelGrant-Libato <michaelgrantlibato7@gmail.com>
2025-09-26 15:37:37 +0800

    feature(app_StudyHive): setup templates

--- StudyHive/StudyHive/settings.py
+++ StudyHive/StudyHive/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'StudyHive.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/app_StudyHive'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
